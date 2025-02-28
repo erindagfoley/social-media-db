f2e7a36898b3c17af099d11e3fd42be12cd00406
erin foley <erindagfoley@gmail.com>
2025-02-27 20:55:21 -0500

    updated data.ts and index.ts, but getting terminal errors

--- Develop/src/seeds/index.ts
+++ Develop/src/seeds/index.ts
@@ -1,7 +1,7 @@
 import db from '../config/connection.js';
-import { Thought, User } from '../models/index.js';
+import { User } from '../models/index.js';
 import cleanDB from './cleanDB.js';
-import { getRandomName, getRandomReaction } from './data.js'; //not sure what random name should be changed to
+import { getRandomUsername } from './data.js'; //not sure what random name should be changed to
 
 try {
   await db();
@@ -12,34 +12,19 @@ try {
 
   // Loop 20 times -- add users to the users array
   for (let i = 0; i < 20; i++) {
-    // Get some random reaction objects using a helper function that we imported from ./data
-    const reactions = getRandomReactions(20);
-//not sure what full name, first, last, and github should be changed to
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-//not sure what first, last, and github should be changed to
+    // Get some random username objects using a helper function that we imported from ./data
+    const username = getRandomUsername();
     users.push({
-      first,
-      last,
-      github,
-      reactions,
+      username
     });
   }
 
   // Add users to the collection and await the results
   const userData = await User.create(users);
 
-  // Add thoughts to the collection and await the results
-  await Thought.create({
-    name: 'SpongeBob is the best cartoon ever',
-    inPerson: false, //not sure what to change inPerson to
-    users: [...userData.map(({ _id }: { [key: string]: any }) => _id)],
-  });
 
   // Log out the seed data to indicate what should appear in the database
-  console.table(users);
+  console.table(userData);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 } catch (error) {
