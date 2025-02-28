dd5d381ef991a1b4b34958ac36c8936e7f5c65b4
erin foley <erindagfoley@gmail.com>
2025-02-27 19:51:10 -0500

    changed code on user model to more closely match the thought model

--- Develop/src/models/User.ts
+++ Develop/src/models/User.ts
@@ -1,80 +1,45 @@
-import { Schema, Types, model, type Document } from 'mongoose';
-
-interface IReaction extends Document {
-    reactionId: Schema.Types.ObjectId,
-    name: string, 
-    //not sure what we should replace score with, if anything
-    score: number
-}
+import { Schema, model, type Document } from 'mongoose';
 
 interface IUser extends Document {
     username: string,
     email: string,
     thoughts: Schema.Types.ObjectId[],
-    friends: Schema.Types.ObjectId[],
-    friendCount?: string 
+    friends: Schema.Types.ObjectId[]
 }
 
-const reactionSchema = new Schema<IReaction>( //schema only
+const userSchema = new Schema<IUser>(
     {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        //not sure what to replace score with
         username: {
             type: String,
             required: true,
-            maxlength: 50,
-            minlength: 4,
-            default: 'Unnamed user',
+            unique: true,
         },
-        score: {
-            type: Number,
+        email: {
+            type: String,
             required: true,
-            default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+            unique: true,
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+        friends: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
         },
     },
-    {
-        timestamps: true,
-        _id: false
-    }
-);
-
-const userSchema = new Schema<IUser>({
-    username: {
-        type: String,
-        required: true,
-        max_length: 50,
-    },
-    email: {
-        type: String,
-        required: true,
-        max_length: 50,
-    },
-    thoughts: {
-        type: String,
-        required: true,
-        max_length: 50,
-    },
-    friends: [thoughtSchema], //thought model will have a subdoc of reactions. user model will not
-},
     {
         toJSON: {
-            getters: true,
+            virtuals: true,
         },
-        timestamps: true
-    }
+        id: false
+    },
 );
 
-userSchema.virtual('friendCount').
-  get(function () { return `$(this.length)`; }).
-  set(function (v) { //v is a banana; however it makes sense because it stands for virtual
-    const friendsLength = v.subdoc(0, v.indexOf(' ')); //substring makes it so that it makes the firstName and lastName are split up in the full name. In addition, indexOf will give the firstName and the lastName an index number. lastName is the index after firstName; hence the + 1 in line 30
-    const lastName = v.substring(v.indexOf(' ') + 1);
-    this.set({ friends: friendsLength })
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
   });
 
-const User = model('User', userSchema);
+const User = model<IUser>('User', userSchema);
 
 export default User;
